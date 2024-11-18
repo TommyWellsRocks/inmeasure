764bc3be4d207c180f5a1e5b00beec68e680912f
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-11-18 13:42:32 -0600

    Move scripts into public folder

--- src/server/types/tiers.ts
+++ src/server/types/tiers.ts
@@ -1,7 +1,7 @@
 export const scripts = {
-  bronze: "data/scripts/listener/bronzeScript.js",
-  silver: "data/scripts/listener/silverScript.js",
-  gold: "data/scripts/listener/goldScript.js",
+  bronze: "public/scripts/listener/bronzeScript.js",
+  silver: "public/scripts/listener/silverScript.js",
+  gold: "public/scripts/listener/goldScript.js",
 };
 
 export type AnalyticsLevelType = "bronze" | "silver" | "gold";
