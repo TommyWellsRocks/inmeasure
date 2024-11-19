99a1dca2f446aa06ad8f124b9b2c5ead64a40d65
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-11-19 10:19:14 -0600

    InMeasure logo

--- src/components/layout/HeaderNav/HeaderLogo.tsx
+++ src/components/layout/HeaderNav/HeaderLogo.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function HeaderLogo() {
   return (
     <Link href="/" className="hover:pointer text-3xl font-bold">
-      InMeasure
+      <Image
+        priority
+        alt="InMeasure Logo"
+        src="/InMeasureLogo.png"
+        width={200}
+        height={23}
+      />
     </Link>
   );
 }
