936f2d280a6567652ec062eedcc5ed4c0b6df6b6
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-11-23 10:10:35 -0600

    Dashboard. Add prev10Minute users. Delete prev5Years users. Last 30 seconds is activeUsers.

--- src/components/dashboard/TotalVisitorsTable.tsx
+++ src/components/dashboard/TotalVisitorsTable.tsx
@@ -6,29 +6,38 @@ export function TotalVisitorsTable({
   connectionTimestamps: number[];
 }) {
   let currentActiveUsers = 0;
+  let prev10MinUsers = 0;
   let prevHourUsers = 0;
   let prev24HourUsers = 0;
   let prev7DayUsers = 0;
   let prev30DayUsers = 0;
   let prev6MonthUsers = 0;
   let prev1YearUsers = 0;
-  let prev5YearUsers = 0;
 
   const now = Date.now();
 
   connectionTimestamps.forEach((timestamp) => {
     const howLongAgo = now - timestamp;
 
-    // If in the last minute, activeUsere
-    if (howLongAgo <= timeToMS(1, "minutes")) {
+    // If in the 30 seconds, activeUsere
+    if (howLongAgo <= timeToMS(30, "seconds")) {
       currentActiveUsers++;
+      prev10MinUsers++;
       prevHourUsers++;
       prev24HourUsers++;
       prev7DayUsers++;
       prev30DayUsers++;
       prev6MonthUsers++;
       prev1YearUsers++;
-      prev5YearUsers++;
+      // If in the last 10 minutes
+    } else if (howLongAgo <= timeToMS(10, "minutes")) {
+      prev10MinUsers++;
+      prevHourUsers++;
+      prev24HourUsers++;
+      prev7DayUsers++;
+      prev30DayUsers++;
+      prev6MonthUsers++;
+      prev1YearUsers++;
     }
     // In the last hour
     else if (howLongAgo <= timeToMS(1, "hours")) {
@@ -38,7 +47,6 @@ export function TotalVisitorsTable({
       prev30DayUsers++;
       prev6MonthUsers++;
       prev1YearUsers++;
-      prev5YearUsers++;
     }
     // In the last 24 hours
     else if (howLongAgo <= timeToMS(1, "days")) {
@@ -47,7 +55,6 @@ export function TotalVisitorsTable({
       prev30DayUsers++;
       prev6MonthUsers++;
       prev1YearUsers++;
-      prev5YearUsers++;
     }
     // In the last 7 days
     else if (howLongAgo <= timeToMS(7, "days")) {
@@ -55,41 +62,33 @@ export function TotalVisitorsTable({
       prev30DayUsers++;
       prev6MonthUsers++;
       prev1YearUsers++;
-      prev5YearUsers++;
     }
     // In the last 30 days
     else if (howLongAgo <= timeToMS(30, "days")) {
       prev30DayUsers++;
       prev6MonthUsers++;
       prev1YearUsers++;
-      prev5YearUsers++;
     }
     // In the last 6 months
     else if (howLongAgo <= timeToMS(6, "months")) {
       prev6MonthUsers++;
       prev1YearUsers++;
-      prev5YearUsers++;
     }
     // In the last year
     else if (howLongAgo <= timeToMS(1, "years")) {
       prev1YearUsers++;
-      prev5YearUsers++;
-    }
-    // In the last 5 years
-    else if (howLongAgo <= timeToMS(5, "years")) {
-      prev5YearUsers++;
     }
   });
 
   const items = [
     { col1: "Online", col2: currentActiveUsers },
+    { col1: "Last 10 Minutes", col2: prev10MinUsers },
     { col1: "Last Hour", col2: prevHourUsers },
     { col1: "Last 24 Hours", col2: prev24HourUsers },
     { col1: "Last 7 Days", col2: prev7DayUsers },
     { col1: "Last 30 Days", col2: prev30DayUsers },
     { col1: "Last 6 Months", col2: prev6MonthUsers },
     { col1: "Last 1 Year", col2: prev1YearUsers },
-    { col1: "Last 5 Years", col2: prev5YearUsers },
   ];
 
   return (
