6644e4c8ee58008a4c7d3ddc018c7a6bc4a5037b
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-11-25 10:54:20 -0600

    StandardAPI logging and error handling

--- src/server/db/queries/apis/standardAPI.ts
+++ src/server/db/queries/apis/standardAPI.ts
@@ -12,20 +12,45 @@ export async function authorizeAndWriteMessage(
   apiKey: string,
   connectionId: string,
 ) {
+  const response = { responseStatus: 201, responseMessage: "" };
   await db.transaction(async (dbPen) => {
     // Get the organizationId where isOrganization and isConnectionId
-    const organization = await getOrg(dbPen, domain, apiKey, connectionId);
+    const organization = await getOrg(
+      dbPen,
+      domain,
+      apiKey,
+      connectionId,
+    ).catch((err) => {
+      console.error(
+        `StandardAPI - getOrg Authorization Error: ${err} - Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
+      );
+      response.responseStatus = 500;
+    });
 
     const organizationId = organization?.id;
     if (organizationId) {
-      await dbPen.insert(standardMessages).values({
-        ...data,
-        connectionId,
-        organizationId,
-        timeToPageLoad: String(data.timeToPageLoad),
-        timeToFirstInteraction: String(data.timeToFirstInteraction),
-        perfTimestamp: String(data.perfTimestamp),
-      });
+      await dbPen
+        .insert(standardMessages)
+        .values({
+          ...data,
+          connectionId,
+          organizationId,
+          timeToPageLoad: String(data.timeToPageLoad),
+          timeToFirstInteraction: String(data.timeToFirstInteraction),
+          perfTimestamp: String(data.perfTimestamp),
+        })
+        .catch((err) => {
+          console.error(
+            `StandardAPI - Insert Standard Message Error: ${err} - Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
+          );
+          response.responseStatus = 500;
+        });
+    } else {
+      console.warn(
+        `StandardAPI - Illegal Organization Attempt. Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
+      );
+      response.responseStatus = 403;
     }
   });
+  return response;
 }
