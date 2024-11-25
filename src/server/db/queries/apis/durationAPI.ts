e3b4ea24a5d750c5458d1e817a09e1948e5650e7
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-11-25 10:09:59 -0600

    DurationAPI logging and error handling

--- src/server/db/queries/apis/durationAPI.ts
+++ src/server/db/queries/apis/durationAPI.ts
@@ -2,10 +2,9 @@ import "server-only";
 
 import { db } from "~/server/db";
 import { getOrg } from "../helpers/getOrg";
-import { durationMessages } from "../../schema";
+import { durationMessages, pageURLEntries } from "../../schema";
 
 import type { DurationMessage } from "~/server/types/scripts";
-import { pageURLEntries } from "../../schemas/analyticsTables/pageURLEntries";
 
 export async function authorizeAndWriteMessage(
   data: DurationMessage,
@@ -13,24 +12,61 @@ export async function authorizeAndWriteMessage(
   apiKey: string,
   connectionId: string,
 ) {
+  const response = { responseStatus: 201, responseMessage: "" };
   await db.transaction(async (dbPen) => {
     // Get the organizationId where isOrganization and isConnectionId
-    const organization = await getOrg(dbPen, domain, apiKey, connectionId);
+    const organization = await getOrg(
+      dbPen,
+      domain,
+      apiKey,
+      connectionId,
+    ).catch((err) => {
+      console.error(
+        `DurationAPI - getOrg Authorization Error: ${err} - Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
+      );
+      response.responseStatus = 500;
+    });
 
     const organizationId = organization?.id;
     if (organizationId) {
+      // Insert Duration Message
       const durationMessage = dbPen
         .insert(durationMessages)
-        .values({ connectionId, organizationId, timestamp: data.timestamp });
-      const pageMessages = data.pageURL.map((entry) =>
-        dbPen.insert(pageURLEntries).values({
+        .values({ connectionId, organizationId, timestamp: data.timestamp })
+        .catch((err) => {
+          console.error(
+            `DurationAPI - Insert Duration Message Error: ${err} - Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
+          );
+          response.responseStatus = 500;
+        });
+
+      const pageMessageValues = data.pageURL
+        .filter((entry) => entry.url)
+        .map((entry) => ({
           connectionId,
           organizationId,
           pageURL: entry.url,
           perfTimestamp: String(entry.perfTimestamp),
-        }),
+        }));
+
+      // Insert PageURL Messages
+      const pageMessages = dbPen
+        .insert(pageURLEntries)
+        .values(pageMessageValues)
+        .catch((err) => {
+          console.error(
+            `DurationAPI - Insert PageURL Messages Error: ${err} - Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
+          );
+          response.responseStatus = 500;
+        });
+
+      await Promise.all([durationMessage, pageMessages]);
+    } else {
+      console.warn(
+        `DurationAPI - Illegal Organization Attempt. Domain: ${domain}, APIKEY: ${apiKey}, ConnectionId: ${connectionId}.`,
       );
-      await Promise.all([durationMessage, ...pageMessages]);
+      response.responseStatus = 403;
     }
   });
+  return response;
 }
